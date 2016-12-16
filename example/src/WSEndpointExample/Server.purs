6615d2d30f8a1a317539bcd908f61cf25c5338ac
FrigoEU <simon.van.casteren@gmail.com>
2016-12-16 11:09:52 +0100

    fix bower

--- example/src/WSEndpointExample/Server.purs
+++ example/src/WSEndpointExample/Server.purs
@@ -11,19 +11,10 @@ module WSEndpointExample.Server where
 -- import WSEndpointExample.Model (Order(Order), getOrdersEndpoint)
 
 -- import Node.Express.Endpoint (EXPRESS, listen, hostStatic, hostEndpoint, makeApp)
+import Node.WebSocket
 
 -- ----------------------------
 
--- myOrders :: Array Order
--- myOrders = [Order {productId: 1, quantity: 50}, Order {productId: 2, quantity: 6}]
+main = do
+  log "ab"
 
--- main :: forall eff. Eff ( console :: CONSOLE, express :: EXPRESS, ajax :: AJAX | eff ) Unit
--- main = do
---   app <- makeApp
---   hostEndpoint app getOrdersEndpoint (\productId _ -> return $ filterOrders productId)
---   hostStatic app "static"
---   listen app 8080
---   log "Listening on port 8080"
-
--- filterOrders :: Int -> Array Order
--- filterOrders id = filter (\(Order {productId}) -> productId == id) myOrders
